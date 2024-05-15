f999cfb0836a2a24f60d1750fde4915913127e23
Пользователь <Subhon>
2024-05-15 19:03:02 +0300

    dsfgs

---
+++ bin/Task1.dart
@@ -0,0 +1,35 @@
+import 'package:qwertyt/qwertyt.dart' as qwertyt;
+import 'dart:io';
+
+void main() {
+// List<int> arr = [];
+// int n, sum = 0;
+// n = int.parse(stdin.readLineSync()!);
+// for (int i = 0; i < n; i++) {
+//   arr.add(int.parse(stdin.readLineSync()!));
+// }
+// for (int i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+// }
+// stdout.write(sum);
+
+
+int a, b, res = 0;
+a = int.parse(stdin.readLineSync()!);
+b = int.parse(stdin.readLineSync()!);
+
+for (int i = 1; i <= a; i++) {
+res = res + b; 
+  
+}
+// stdout.write("$a * $b = $res");
+print(a * b);
+
+
+
+
+
+
+  
+}
+
